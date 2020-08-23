381c824edc5e40f2d1af525298c4fcc0991f3207
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-23 19:54:51 -0300

    ajustando função formulario

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -17,9 +17,14 @@ close_nav.onclick = function () {
 //form newsletter
 let button_newsletter = document.querySelector(".newsletter-form .action");
 let input_newsletter = document.querySelector(".newsletter-form .input");
+let email_regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 button_newsletter.onclick = function (e) {
     e.preventDefault();
-    alert("Assinatura concluída com sucesso!");
+    if (input_newsletter.value !== "" && input_newsletter.value.match(email_regex)) {
+        alert("Assinatura concluída com sucesso!");
+    } else {
+        alert("Email inválido, tente novamente");
+    }
     input_newsletter.value = "";
 }
