bef300e8097aaa7524b38037bc1e894584ffd9ee
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-23 19:44:49 -0300

    removed folder

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,4 @@
+//menu
 let navigation = document.querySelector(".nav-menu");
 let button_nav = document.querySelector(".nav-mobile");
 let close_nav = document.querySelector(".nav-close");
@@ -11,4 +12,14 @@ button_nav.onclick = function () {
 close_nav.onclick = function () {
     navigation.classList.remove("-visible");
     body.classList.remove("-open");
+}
+
+//form newsletter
+let button_newsletter = document.querySelector(".newsletter-form .action");
+let input_newsletter = document.querySelector(".newsletter-form .input");
+
+button_newsletter.onclick = function (e) {
+    e.preventDefault();
+    alert("Assinatura concluída com sucesso!");
+    input_newsletter.value = "";
 }
